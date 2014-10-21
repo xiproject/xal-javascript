ced06ab379a21d8c215a4bed9a5abe7ac7f97646
Mihir Rege <mihirrege@gmail.com>
2014-10-21 05:45:03 +0530

    Add eventListeners, subscribe and register function

--- xal.js
+++ xal.js
@@ -1,59 +1,107 @@
 var restify = require('restify');
 var config = require('./config.json');
 var bunyan = require('bunyan');
-var server = require('server');
-var log = server.log;
+var log = require('./log.js');
 
 var xiClient = restify.createJsonClient({
-  url: config['xi-core'].url,
-  version: '*',
-  log: log
+    url: config['xi-core'].url,
+    version: '*',
+    log: log
 });
+
+/* XAL State */
+var id;
+var name;
+var eventListeners = {};
 
 /* Register with xi-core */
 function register(data, cb) {
-  xiClient.post('/register', data, function(err, response, body) {
-    if (err) {
-      log.error(err);
-      cb(err);
-      return;
-    }
-    log.debug({response: response, body: body});
-    log.info('Registered with xi-core');
-    cb(null, body.id);
-  });
+    xiClient.post('/register', data, function(err, request, response, body) {
+        if (err) {
+            log.error(err);
+            if(cb)
+                cb(err);
+            return;
+        }
+        id = body.id;
+        log.info('Registered with xi-core');
+        log.info("Received uuid ", id);
+        if(cb)
+            cb(null, body.id);
+    });
+}
+
+/* Subscribe events with xi-core 
+   Agent has to be registered with xi-core before subscribing
+   data = { id: id , events: [] }
+*/
+
+function subscribe(data, cb){
+    xiClient.post('/subscribe', data, function(err, request, response, body){
+        if(err){
+            log.error(err);
+            if(cb)
+                cb(err);
+            return;
+        }
+        log.info("Subscribed successfully");
+        if(cb)
+            cb(null,body.id);
+    });
 }
 
 function ping(cb) {
-  xiClient.get('/ping', function(err, response, body) {
-    if (err) {
-      log.error(err);
-      cb(err);
-      return;
+    xiClient.get('/ping', function(err, request, response, body) {
+        if (err) {
+            log.error(err);
+            if(cb)
+                cb(err);
+            return;
+        }
+        log.debug({response: response});
+        if(cb)
+            cb();
+    });
+}
+
+function on(event, cb) {
+    if (eventListeners.hasOwnProperty(event)) {
+        eventListeners[event].push(cb);
+    } else {
+        eventListeners[event] = [];
+        eventListeners[event].push(cb);
     }
-    log.debug({response: response});
-    cb();
-  });
+    log.debug("Registered new callback for event: " + event);
 }
 
-var messageListeners = {};
 
-function onMessage(channel, cb) {
-  if (messageListeners.hasOwnProperty(channel)) {
-    messageListeners[channel].push(cb);
-  } else {
-    messageListeners[channel] = [];
-    messageListeners[channel].push(cb);
-  }
-  log.debug("Registered new callback for channel: " + channel);
+function setName(data){
+    name = data;
 }
 
-// Test
+function start(err, cb){
+    log.info("Registering agent with name ", name);
+    register({ name: name},
+             function(err){
+                 if(err){
+                     log.error(err);
+                     if(cb)
+                         cb(err);
+                     return;
+                 }
+                 else{
+                     var keys = [];
+                     for(var k in eventListeners){
+                         if(eventListeners.hasOwnProperty(k))
+                             keys.push(k);
+                     }
+                     var data = { id: id , events: keys};
+                     log.info("Subscribing using", data);
+                     subscribe( data);
+                 }                     
+             });
+}
 
-register({
-  name: 'test',
-  subscribe: 'xi.input.speech'
-});
 function messageHandler(req , res ,next){
     if(eventListeners[req.body.event]){
         for( var i =0; i < eventListeners[req.body.event].length ; i++){
@@ -64,4 +112,9 @@ function messageHandler(req , res ,next){
     res.send(200);
     next();
 }
+
+
+module.exports.on = on;
+module.exports.setName = setName;
+module.exports.start = start;
 module.exports.messageHandler = messageHandler;
