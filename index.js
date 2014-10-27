4dcc6066383daddf2ee147e3a3e55def646571d6
Vikrant Varma <vikrant.varma94@gmail.com>
2014-10-28 00:52:56 +0530

    Export improved start() from index - server + xal

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+'use strict';
+
 var xal = require('./lib/xal');
+var server = require('./lib/server');
 
 module.exports = xal;
+
+var xalStart = xal.start;
+
+module.exports.start = function(data, cb) {
+    server.start(function() {
+        xalStart(data, cb);
+    });
+};
