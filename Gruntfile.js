5b89e41f119fdebc5ef3963bcb447573df0112fe
Mihir Rege <mihirrege@gmail.com>
2014-11-06 23:22:59 +0530

    Remove comma

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
             },
             options: {
                 jshintrc: true
-            },
+            }
         },
         jsbeautifier: {
             default: {
