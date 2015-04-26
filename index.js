20970cd1437aab9c6a4b5df0ad1403c12fb7a895
Vikrant Varma <vikrant.varma94@gmail.com>
2015-04-26 10:52:49 +0530

    Demo 1

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var xal = require('./lib/xal');
 var server = require('./lib/server');
+var config = require('./config');
+var io = require('socket.io-client');
 
 module.exports = xal;
 
@@ -9,6 +11,8 @@ var xalStart = xal.start;
 
 module.exports.start = function(data, cb) {
     server.start(function() {
+        var socket = io.connect('http://0.0.0.0:9000');
+        socket.emit('registration', data);
         xalStart(data, cb);
     });
 };
